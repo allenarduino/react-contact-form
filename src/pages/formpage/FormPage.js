31b8606e257d8a6b8c45deae99428f4c3b01881c
ALLEN <aljay3334@gmail.com>
2023-02-14 16:10:47 +0000

    Added the pages and react-router-dom component

---
+++ src/pages/formpage/FormPage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const FormPage = () => {
+  return <h1>This is the form page</h1>;
+};
+
+export default FormPage;
---
+++ src/pages/redirectpage/RedirectPage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const RedirectPage = () => {
+  return <h1>Redirect Page</h1>;
+};
+
+export default RedirectPage;
